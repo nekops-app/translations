e5e8e58a6892bd2953013d88392c37828f0694e2
Nya Candy <dev@candinya.com>
2025-05-04 14:13:35 +0800

    feat: split constants

---
+++ constants.ts
@@ -0,0 +1,7 @@
+export const supportedLngs = ["en-US", "zh-CN"];
+export const fallbackLng = supportedLngs[0];
+
+export const languageName = {
+  "en-US": "English (United States)",
+  "zh-CN": "中文（简体）",
+};
